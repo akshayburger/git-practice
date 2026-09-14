be801d9d6a103c4070def67b41fbaa1b6ed6ac53
akshayburger <akshayvignesh000@gmail.com>
2026-09-14 19:14:36 +0530

    some changes made: folder plus editted

--- templates/calculator.py
+++ templates/calculator.py
@@ -2,11 +2,17 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
+
+@app.route("/")
+def home():
+    return render_template("calculator_UI.html")
+
+
 @app.route("/calculate", methods=["POST"])
 def calculate():
 
     a = int(request.form["a"])
-    b = int(request.form["b"])   
+    b = int(request.form["b"])
     operation = request.form["operation"]
 
     if operation == "add":
@@ -17,9 +23,15 @@ def calculate():
 
     elif operation == "multiply":
         result = a * b
+
     elif operation == "divide":
-        result = a / b  if b != 0 else "Error: Division by zero"   
+        result = a / b if b != 0 else "Error: Division by zero"
+
     else:
         result = "Invalid operation"
 
     return render_template("calculator_UI.html", result=result)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
